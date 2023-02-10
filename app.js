2a5969af12a46012ef2b59365adc2c11af6d8124
GUILHERME RUFINO CAMPOS <22282090@CFP127ALUNO.LOCAL>
2023-02-10 07:55:32 -0300

    inicio entrada de dados

--- app.js
+++ app.js
@@ -15,3 +15,32 @@
 
 //entradaDados.question('Escolha o mutiplicador') {}
 
+var matematica = require('./modulo/tabuada.js');
+
+//import da biblioteca para entrada de dados
+var readline = require('readline');
+
+//criar objeto para receber dados via teclado
+
+var entradaDados = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+
+
+});
+
+//pesuisar 
+
+entradaDados.question(' qual é multilicado?: \n', function(numero1) {
+    let valor1 = numero1.replace(',', '.');
+    entradaDados.question(' qual é multilicando?: \n', function(numero2) {
+        let valor2 = numero2.replace(',', '.');
+
+
+
+    });
+
+
+
+
+});
